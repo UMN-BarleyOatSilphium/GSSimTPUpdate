e7d7832eb4cfdfce065597dbf47c2f6bbe70e030
Jeff Neyhart <neyhartje@gmail.com>
2016-10-02 15:00:33 -0500

    Edits to the results parser

--- R/parse_results.R
+++ R/parse_results.R
@@ -25,26 +25,25 @@ parse.results <- function(files, filename) {
     # Load the file. This will be filled in later
     load(f)
     
-    # Check to make sure that all of the elements of the results list are lists, too
-    check.lists <- sapply(experiment.sub.results, is.list)
+    # Unlist the first layer of the results list
+    experiment.sub.results <- experiment.sub.results %>% 
+      unlist(recursive = F)
     
-    # If one of the elements is not a list
-    if (any(!check.lists)) {
-      cat("\nOne of the elements in the results list is not a list. An error 
-          likely occured.\n")
-      cat("\nFilename: ", f)
-      cat("\nElement index: ", which(!check.lists))
-      cat("\nContents of element:\n")
-      print(experiment.sub.results[!check.lists])
+    # Remove any rep results that are not lists
+    check.lists <- sapply(X = experiment.sub.results, FUN = is.list)
+    experiment.sub.results[!check.lists] <- NULL
+    
+    if (sum(!check.lists) > 0) {
+    
+      # Report the number of reps that were discarded
+      cat("\n\nReps were removed from the file.")
+      cat("\n\nFilename: ", f)
+      cat("\nNumber of reps removed: ", sum(!check.lists))
       
-      # Remove the element from the list
-      experiment.sub.results[!check.lists] <- NULL
-      
-    } # Close the if statement
-      
+    }
     
     # Find the number of cycles
-    n.cycles <- experiment.sub.results[[1]][[1]]$sim.results %>%
+    n.cycles <- experiment.sub.results[[1]]$sim.results %>%
       length()
     
     # Create an empty list to save
@@ -52,123 +51,109 @@ parse.results <- function(files, filename) {
     
 
     # Genetic variance of the candidates
-    save.list[["sc.gen.var"]] <- lapply(X = experiment.sub.results, FUN = function(set)
-      lapply(X = set, FUN = function(rep)
+    save.list[["sc.gen.var"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
         lapply(X = rep$sim.result, FUN = function(cycle)
-          return(cycle$candidate.values$true.var.components$V_g) ))) %>%
+          return(cycle$candidate.values$true.var.components$V_g) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
 
     # Genotypic value of candidates
-    save.list[["sc.gen.val"]] <- lapply(X = experiment.sub.results, FUN = function(set) {
-      lapply(X = set, FUN = function(rep) {
-        lapply(X = rep$sim.result, FUN = function(cycle) {
-          return(cycle$candidate.values$mu.g) })})}) %>%
+    save.list[["sc.gen.val"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
+          return(cycle$candidate.values$mu.g) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
 
 
 
     # Allele frequencies
-    save.list[["sc.allele.freq"]] <- lapply(X = experiment.sub.results, FUN = function(set) {
-      lapply(X = set, FUN = function(rep) {
-        lapply(X = rep$sim.result, FUN = function(cycle) {
+    save.list[["sc.allele.freq"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
           list(qtl = cycle$geno.summary.stats$candidate.af$qtl,
-               markers = cycle$geno.summary.stats$candidate.af$snp) })})}) %>%
+               markers = cycle$geno.summary.stats$candidate.af$snp) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
 
-    save.list[["TP.allele.freq"]] <- lapply(X = experiment.sub.results, FUN = function(set) {
-      lapply(X = set, FUN = function(rep) {
-        lapply(X = rep$sim.result, FUN = function(cycle) {
+    save.list[["TP.allele.freq"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
           list(qtl = cycle$geno.summary.stats$TP.af$qtl,
-               markers = cycle$geno.summary.stats$TP.af$snp) })})}) %>%
+               markers = cycle$geno.summary.stats$TP.af$snp) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
 
     # Pairwise LD
-    save.list[["qtl.marker.LD"]] <- lapply(X = experiment.sub.results, FUN = function(set)
-      lapply(X = set, FUN = function(rep)
-        lapply(X = rep$sim.result, FUN = function(cycle)
+    save.list[["qtl.marker.LD"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
           list(mean_max_genome = cycle$geno.summary.stats$qtl.marker.LD$mean.max.genome,
-               persistence = cycle$geno.summary.stats$qtl.marker.LD$persistance.of.phase )))) %>%
+               persistence = cycle$geno.summary.stats$qtl.marker.LD$persistance.of.phase ))) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
     
     # Number of qtl and markers used to measure LD
-    save.list[["n.loci.LD"]] <- lapply(X = experiment.sub.results, FUN = function(set)
-      lapply(X = set, FUN = function(rep)
-        lapply(X = rep$sim.result, FUN = function(cycle)
+    save.list[["n.loci.LD"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
           list(n_qtl = cycle$geno.summary.stats$qtl.marker.LD$n.qtl.LD,
-               n_markers = cycle$geno.summary.stats$qtl.marker.LD$n.marker.LD) ))) %>%
+               n_markers = cycle$geno.summary.stats$qtl.marker.LD$n.marker.LD) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
 
     # Prediction accuracy results
-    save.list[["validation.results"]] <- lapply(X = experiment.sub.results, FUN = function(set) {
-      lapply(X = set, FUN = function(rep) {
-        lapply(X = rep$sim.result, FUN = function(cycle) {
-          return(cycle$prediction.accuracy) })})}) %>%
+    save.list[["validation.results"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
+          return(cycle$prediction.accuracy) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
 
     # Training population updating - expected heterozygosity
-    save.list[["tp.update.exp.het"]] <- lapply(X = experiment.sub.results, FUN = function(set) {
-      lapply(X = set, FUN = function(rep) {
-        lapply(X = rep$sim.result, FUN = function(cycle) {
-          return(cycle$tp.update$Exp.het) })})}) %>%
+    save.list[["tp.update.exp.het"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
+          return(cycle$tp.update$Exp.het) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
     
     # Determine the effect of the 1 allele for each QTL over iterations
-    save.list[["qtl.effects"]] <- lapply(X = experiment.sub.results, FUN = function(set) 
-      lapply(X = set, FUN = function(rep) 
-        qtl.eff(rep$genome)$add )) %>%
+    save.list[["qtl.effects"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      qtl.eff(rep$genome)$add ) %>%
       as.data.frame() %>%
       tbl_df() %>%
       gather(iter, value) %>%
       mutate(iter = iter %>% as.factor() %>% as.numeric())
       
     ## Inbreeding
-    save.list[["sc.inbreeding"]] <- lapply(X = experiment.sub.results, FUN = function(set) {
-      lapply(X = set, FUN = function(rep) {
-        lapply(X = rep$sim.result, FUN = function(cycle) {
-          return(cycle$inbreeding$candidates) })})}) %>%
+    save.list[["sc.inbreeding"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
+          return(cycle$inbreeding$candidates) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
     
-    save.list[["tp.additions.inbreeding"]] <- lapply(X = experiment.sub.results, FUN = function(set) {
-      lapply(X = set, FUN = function(rep) {
-        lapply(X = rep$sim.result, FUN = function(cycle) {
-          return(cycle$inbreeding$TP.additions) })})}) %>%
+    save.list[["tp.additions.inbreeding"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
+          return(cycle$inbreeding$TP.additions) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
     
     ## Relationships
-    save.list[["tp.sc.relationship"]] <- lapply(X = experiment.sub.results, FUN = function(set) {
-      lapply(X = set, FUN = function(rep) {
-        lapply(X = rep$sim.result, FUN = function(cycle) {
-          return(cycle$relationship$TP.candidates) })})}) %>%
+    save.list[["tp.sc.relationship"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
+          return(cycle$relationship$TP.candidates) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
     
-    save.list[["tp.additions.relationship"]] <- lapply(X = experiment.sub.results, FUN = function(set) {
-      lapply(X = set, FUN = function(rep) {
-        lapply(X = rep$sim.result, FUN = function(cycle) {
-          return(cycle$relationship$TP.additions) })})}) %>%
+    save.list[["tp.additions.relationship"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
+          return(cycle$relationship$TP.additions) )) %>%
       unlist() %>%
       GSsim.TPUpdate:::nv_df(change = change)
     
     # Results of the persistence of LD permutation tests
-    save.list[["permutation.results"]] <- lapply(X = experiment.sub.results, FUN = function(set) 
-      lapply(X = set, FUN = function(rep) 
-        lapply(X = rep$sim.result, FUN = function(cycle) 
-          return(cycle$geno.summary.stats$qtl.marker.LD$persistence.perm.results) )))
+    save.list[["permutation.results"]] <- lapply(X = experiment.sub.results, FUN = function(rep)
+      lapply(X = rep$sim.result, FUN = function(cycle)
+          return(cycle$geno.summary.stats$qtl.marker.LD$persistence.perm.results) ))
   
     # Build a list
     collective.abbreviated.results[[change]] <- save.list
     
-    cat("File: ", f, " parsed.\n\n")
+    cat("\n\nFile: ", f, " parsed.\n\n")
     
     
   } # Close the for loop
